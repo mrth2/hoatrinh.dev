fc023acebbb725a2ccd689b567ecb23732a50b15
kyle <mr.th2.90@gmail.com>
2021-05-16 18:56:59 +0700

    init nuxt

---
+++ pages/posts/index.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+  scrollToTop: true,
+  data () {
+    return {
+      postPage: {},
+      posts: []
+    }
+  },
+  async fetch () {
+    this.posts = await this.$axios.get('/posts')
+    this.postPage = await this.$axios.get('/posts-page')
+  }
+}
+</script>
