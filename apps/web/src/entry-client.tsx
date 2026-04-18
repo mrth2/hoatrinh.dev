ef837c9aac3ddf6c8d987c4ba6234ae1d70bfce9
mrth2 <mr.th2.90@gmail.com>
2026-04-18 09:09:41 +0700

    feat(web): load Space Mono + JetBrains Mono via fontsource

--- apps/web/src/entry-client.tsx
+++ apps/web/src/entry-client.tsx
@@ -1,3 +1,7 @@
+import './styles/tokens.css';
+import './styles/reset.css';
+import './styles/fonts.css';
+import './styles/global.css';
 import { render } from 'solid-js/web';
 import { App } from './App';
 
