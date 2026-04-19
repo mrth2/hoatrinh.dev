ba946fe4d195b8466deaac479567f7f1b9420859
mrth2 <mr.th2.90@gmail.com>
2026-04-19 17:20:16 +0700

    refactor(web): collapse routes to single wildcard - TerminalPage never remounts

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,19 +1,10 @@
 import { Route, Router } from '@solidjs/router';
-import { NotFoundPage } from './routes/NotFoundPage';
 import { TerminalPage } from './routes/TerminalPage';
 
 export function App(props: { url?: string }) {
   return (
     <Router {...(props.url !== undefined ? { url: props.url } : {})}>
-      <Route path="/" component={TerminalPage} />
-      <Route path="/about" component={TerminalPage} />
-      <Route path="/projects" component={TerminalPage} />
-      <Route path="/project/:slug" component={TerminalPage} />
-      <Route path="/experience" component={TerminalPage} />
-      <Route path="/skills" component={TerminalPage} />
-      <Route path="/contact" component={TerminalPage} />
-      <Route path="/help" component={TerminalPage} />
-      <Route path="*" component={NotFoundPage} />
+      <Route path="/*" component={TerminalPage} />
     </Router>
   );
 }
